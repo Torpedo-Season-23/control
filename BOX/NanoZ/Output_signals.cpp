8e845608719b70300abb4b0f08c768bbf3db6127
youssef-allam <66872519+youssef-allam@users.noreply.github.com>
2023-02-10 22:38:26 +0200

    Update NanoZ

--- BOX/NanoZ/Output_signals.cpp
+++ BOX/NanoZ/Output_signals.cpp
@@ -1,11 +1,11 @@
 #include "Output_signals.h"
 #include "softStart.h"
 
-Servo Motors[motorsCount];
-int motor_controlled_speed[motorsCount] = { MOT_ZERO_SPEED, MOT_ZERO_SPEED, MOT_ZERO_SPEED, MOT_ZERO_SPEED, MOT_ZERO_SPEED, MOT_ZERO_SPEED, MOT_ZERO_SPEED, MOT_ZERO_SPEED };
+Servo Motors[MOTORS_COUNT];
+int motor_controlled_speed[MOTORS_COUNT] = { MOT_ZERO_SPEED};
 
 void Thrusters::init() {
-  for (int i = 0; i < motorsCount; i++) {
+  for (int i = 0; i < MOTORS_COUNT; i++) {
     Motors[i].attach(motorPins[i]);
     Motors[i].writeMicroseconds(MOT_ZERO_SPEED);  //stop speed 1500
   }
@@ -13,21 +13,21 @@ void Thrusters::init() {
 }
 
 void Tools::init() {
-  for (int i = 0; i < toolsCount; i++) {
+  for (int i = 0; i < TOOLS_COUNT; i++) {
     pinMode(toolPins[i], OUTPUT);  //set tool pins to be Output pins
   }
 }
 
 //set signals to tools
 void Tools::apply_signal() {
-  for (int i = 0; i < toolsCount; i++) {
+  for (int i = 0; i < TOOLS_COUNT; i++) {
     analogWrite(toolPins[i], this->tool_frame[i]);
   }
 }
 
 //set signals to Motors
 void Thrusters::apply_signal(int *thrustersSpeeds, float *exponants) {
-  for (int i = 0; i < motorsCount; i++) {
+  for (int i = 0; i < MOTORS_COUNT; i++) {
     //call soft start to control motors speed
     motor_soft_start(Motors[i], thrustersSpeeds[i], (motor_controlled_speed + i), i, exponants[i]);
   }
@@ -36,15 +36,16 @@ void Thrusters::apply_signal(int *thrustersSpeeds, float *exponants) {
 
 //prepare motor speeds and exponants to be sent to motors
 void Thrusters::prep_And_apply_signal() {
-  float exponants[motorsCount];
-  int thrustersSpeeds[motorsCount];
+  float exponants[MOTORS_COUNT];
+  int thrustersSpeeds[MOTORS_COUNT];
 
-  for (int i = 0; i < motorsCount; i++) {
+  for (int i = 0; i < MOTORS_COUNT; i++) {
     int dir = (this->thrustersFrame[i] == 1) ? 1 : -1;
-    int speedChange = map(this->thrustersFrame[i + 8], 0, 255, 0, 400);
+
+    int speedChange = map(this->thrustersFrame[i + MOTORS_COUNT], 0, 255, 0, 400);
     int speedValue = 1500 + dir * speedChange;
     thrustersSpeeds[i] = speedValue;
-    exponants[i] = (int)thrustersFrame[i + 16] / 10.0;
+    exponants[i] = (int)thrustersFrame[i + MOTORS_COUNT*2] / 10.0;
   }
 
   this->apply_signal(thrustersSpeeds, exponants);
--- BOX/NanoZ/UARTTerminal.h
+++ BOX/NanoZ/UARTTerminal.h
@@ -2,10 +2,13 @@
 #define UART_Z_H
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-#define UART_Z_FRAME_SIZE 18// 2 gaurd |1 directions | 6 Motor| 6 exponent | 1 Accessories | 2 gaurd 
-#define ACTUAL_DATA 14 // ( 1 directions | 6 Motor| 6 exponent | 1 Accessories )
-#define THRUSTERSFRAME 13
-#define TOOLSFRAME 8 
+#include "Output_signals.h"
+
+#define UART_Z_FRAME_SIZE 18       // (2 gaurd |1 directions | 6 Motor| 6 exponent | 1 Accessories | 2 gaurd )(bytes)
+#define ACTUAL_DATA 14             // ( 1 directions | 6 Motor| 6 exponent | 1 Accessories )(bytes)
+#define THRUSTERS_FRAME 13         //bytes
+#define THRUSTERS_signal_FRAME 18  //( 6 directions | 6 Motor | 6 exp )(elements)
+#define TOOLS_FRAME 4     
 #define RX_Z 25
 #define TX_Z 24
 
