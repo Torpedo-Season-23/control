2ff801144e8c150630136e6683adbaa49165f130
Menna Magdy <mennamagdyx@gmail.com>
2023-02-19 23:35:09 +0200

    received/sent frames modified

--- Console/communication.cpp
+++ Console/communication.cpp
@@ -1,4 +1,5 @@
 #include "communication.h"
+#include <math.h>
 
 void Communication:: comm_init() {
   Ethernet.begin(this->mac, this->consoleIP);
@@ -16,17 +17,17 @@ void Communication:: receiveData(uint8_t* receivedFrame) {
   }
   if (frameSize = 0)
   {
-    Serial.print("not received");
+    Serial.print("Not received :(");
   }
-  Serial.print("--------");
-   for(int i=0;i<10;i++) Serial.print(receivedFrame[i]); 
-    Serial.println("--------");
+  // Serial.print("--------");
+  //  for(int i=0;i<10;i++) Serial.print(receivedFrame[i]); 
+  //   Serial.println("--------");
 
 }
 
 void Communication:: getSensors(uint8_t* receivedFrame, int16_t* sensors) {  //modify received frame
+  
   // Serial.println("Readings:");
-  // Serial.println("Imu (3 values) -- Pressure (1 value) -- Current (2 values)");  //order of sending needs to be changed in box
   int j = 0;
   for (int i = 0; i < IMU + PRESSURE; i++) {
     sensors[i] = receivedFrame[j+1] + receivedFrame[j] * 256;
@@ -37,43 +38,64 @@ void Communication:: getSensors(uint8_t* receivedFrame, int16_t* sensors) {  //m
     j++;
   }
   // for (int i = 3; i < 4; i++) {
-    Serial.print("pressure");
-    Serial.print(sensors[3]);
-    Serial.println("  ");
+  // Serial.print("pressure");
+  // Serial.print(sensors[3]);
+  // Serial.println("  ");
   // }
 }
 
 void Communication:: prepareData(int* accessories, int* thrusters, uint8_t* sentFrame) {  //modify sent frame
 
+
+// 1st byte for accessories
   int x = 0;
   int weights[8] = { 1, 2, 4, 8, 16, 32, 64, 128 };
   for (int i = ACCESSORIES - 1; i >= 0; i--) {
-    if (accessories[i] == 1) {
+    if (accessories[i] == 1){
       x += weights[i];
     }
   }
   sentFrame[0] = (uint8_t)x;
 
+ // last byte for thrusters' direction
+  x = 0;
+  int weights[6] = { 1, 2, 4, 8, 16, 32};
+  for (int i = THRUSTERS - 1; i >= 0; i--) {
+    if (thrusters[i] > 1500){
+      x += weights[i]; // 1500 will be added in Box
+    }
+  }
+  sentFrame[sentFrameSize - 1] = (uint8_t)x;
 
-  int j = 0;
-  for (int i = 1; i < sentFrameSize; i += 2) {
-    sentFrame[i] = lowByte(thrusters[j]);
-    sentFrame[i + 1] = highByte(thrusters[j]);
+// 6 bytes thrusters' speed
+  int j = 0; // thrusters speed: 1100 - 1900
+  for(int i = 1; i < sentFrameSize-1; i++)
+  {
+    thrusters[j] = abs(thrusters[j] - 1500); //thrusters speed: 0 - 400
+    sentFrame[i] =  map(thrusters[j] , 0, 400, 0, 255); //thrusters speed: 0 - 255
     j++;
   }
-  Serial.print("sent frame ");
-  for (int i = 0; i < 13; i++) {
-    Serial.print(sentFrame[i]);
-    Serial.print(" ");
 
-  }
-  Serial.println();
+  // int j = 0;
+  // for (int i = 1; i < sentFrameSize; i += 2) { //will be changed
+  //   sentFrame[i] = lowByte(thrusters[j]);
+  //   sentFrame[i + 1] = highByte(thrusters[j]);
+  //   j++;
+  // }
+
+
+  // Serial.print("sent frame ");
+  // for (int i = 0; i < 13; i++) {
+  //   Serial.print(sentFrame[i]);
+  //   Serial.print(" ");
+  // }
+  // Serial.println();
 }
 
 void Communication:: sendData(uint8_t* sentFrame) {
   int x = this->udp.beginPacket(this->boxIP, this->boxPort);
   if (!x) {
-    Serial.println("Problem resolving the hostname or port");
+    Serial.println("Problem resolving the hostname or port.");
     this->comm_init();
   }
   this->udp.write(sentFrame, sentFrameSize);
--- Console/config.h
+++ Console/config.h
@@ -2,20 +2,21 @@
 #define CONFIG_H
 
 
-/*---------------Speeds------------------*/
+/*---------------Speeds---------------*/
 #define SPEEDS_NUMBER 3
 #define LOW_SPEED_VALUE 42
 #define NORMAL_SPEED_VALUE 85
 #define HIGH_SPEED_VALUE 128
 
-/*-----------------communication----------------------*/
-#define receivedFrameSize 10  //10 bytes, IMU: 3*2bytes, Pressure: 1*2bytes, Current: 2*1bytes respectively
-#define sentFrameSize 13      //13 bytes, Thrusters: 6*2bytes, Accessories: 1 byte respectively (will be changed if softstart is added)
-#define SENSORS 6
+/*-----------------communication-----------------*/
+#define receivedFrameSize 11  //11 bytes, IMU: 3*2bytes, Pressure: 1*2bytes, Current: 2*1bytes, Leakage: 1byte respectively
+#define sentFrameSize 8      //8 bytes, Thrusters: 7bytes, Accessories: 1 byte respectively (will be changed if softstart is added)
+#define SENSORS 7
 #define IMU 3
-#define CURRENT 2
 #define PRESSURE 1
+#define CURRENT 2
+#define LEAKAGE 1
 #define THRUSTERS 6
-#define ACCESSORIES 4  //will be modified (1 byte anyways)
+#define ACCESSORIES 8  //will be modified (1 byte anyways)
 
 #endif
