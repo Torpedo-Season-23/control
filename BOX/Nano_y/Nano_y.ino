68759231a690d3148ed27bc30eeb07f8838fcce0
Asser <assertamer2002@gmail.com>
2023-02-26 18:18:14 +0200

    fixed

--- BOX/Nano_y/Nano_y.ino
+++ BOX/Nano_y/Nano_y.ino
@@ -1,8 +1,8 @@
 #include "UartX.h"
-#include "SensorsOrganizer.h"
-//TorpedoMPU IMU;
-//PressureSensor pressur_S;
-Sensors mySensors;
+#include "Pressure.h"
+#include "TorpedoMPU.h"
+TorpedoMPU IMU;
+PressureSensor pressur_S;
 Nano_X uart_data;
 
 void handler() {
@@ -17,10 +17,10 @@ long currentTime = millis();
 void setup() {
   Serial.begin(38400);
   uart_data.Start_Uart();
-  //IMU.start();
+  IMU.start();
 
-  //IMU.check();
-  //pressur_S.init();
+  IMU.check();
+  pressur_S.init();
   attachInterrupt(digitalPinToInterrupt(2), handler, LOW);
 }
 void loop() {
@@ -28,8 +28,8 @@ void loop() {
   if (current - currentTime > 100) {
     //noInterrupts();
     //Serial.print("HERE!");
-    uart_data.Set_IMU_Angles(mySensors.getProxyAngles());
-    uart_data.Set_Pressure(mySensors.getProxyPressure());
+    uart_data.Set_IMU_Angles(IMU.getangles());
+    uart_data.Set_Pressure(pressur_S.getPressure());
     currentTime= current;
   }
   //interrupts();
