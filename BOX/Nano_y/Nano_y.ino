87547a2623f00f03b774234c9042197cdc4411cd
Menna Magdy <mennamagdyx@gmail.com>
2023-02-27 17:02:34 +0200

    Leakage Sensor added

--- BOX/Nano_y/Nano_y.ino
+++ BOX/Nano_y/Nano_y.ino
@@ -1,9 +1,14 @@
 #include "UartX.h"
 #include "Pressure.h"
 #include "TorpedoMPU.h"
+#include "Leakage.h"
+#include "Multiplexer8.h"
+
 TorpedoMPU IMU;
 PressureSensor pressur_S;
 Nano_X uart_data;
+LeakageSensor leakSensors[8];
+int sensors_num = 0;
 
 void handler() {
   //Serial.println("In Interrupt!");
@@ -19,6 +24,13 @@ void setup() {
   uart_data.Start_Uart();
   IMU.start();
 
+  init_mux();
+  leakSensors[0].init(); //Q: all wala one? needs to be tested
+
+   for (int i = 0; i < SENSORS_NUM; i++) {
+    leakSensors[i].setSensor(i);
+  }
+
   IMU.check();
   pressur_S.init();
   attachInterrupt(digitalPinToInterrupt(2), handler, LOW);
@@ -31,6 +43,16 @@ void loop() {
     uart_data.Set_IMU_Angles(IMU.getangles());
     uart_data.Set_Pressure(pressur_S.getPressure());
     currentTime= current;
+
+    select_mux(sensors_num); //or for loop for all at once? needs to be tested
+  uint8_t humidity = leakSensors[sensors_num].getHumidity();
+
+  sensors_num++;
+
+  if(sensors_num > 7)
+  {
+    sensors_num = 0;
+  }
   }
   //interrupts();
   Serial.println("Waiting to receive...");
